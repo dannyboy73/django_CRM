2bf087419bd86aed29b2940fce0d49ba00597aa0
dannyboy73 <chris.daniel67@gmail.com>
2020-08-01 16:23:18 +0530

    Profile picture

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import Group
 
 # Create your views here.
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from .decorators import unauthenticated_user, allowed_users, admin_only
 
@@ -87,6 +87,20 @@ def userPage(request):
             'delivered': delivered, 'pending': pending,
     }
     return render(request, 'accounts/user.html', context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+    user = request.user.customer
+    form = CustomerForm(instance=user)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, )
+        if form.is_valid():
+            form.save()
+            
+    context = {'form': form}
+    return render(request, 'accounts/account_settings.html', context)
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
